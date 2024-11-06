fa8e4c38c1e9e8d95b74a85b5865aa67ebf4e00b
Dao Yen <yenn.vcu@gmail.com>
2024-11-06 09:08:11 +0700

    test

--- selenium/webdriver/Topic_01_Environment.java
+++ selenium/webdriver/Topic_01_Environment.java
@@ -14,7 +14,7 @@ public class Topic_01_Environment {
     public void TC_01_Run_On_Firefox() {
         driver = new FirefoxDriver();
         driver.get("https://www.facebook.com/");
-        driver.quit(); //
+        driver.quit();
     }
 
     @Test
