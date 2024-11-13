12cacb3c3f37a2dbda1872fda9e3d23983d8cc23
Dao Yen <yenn.vcu@gmail.com>
2024-11-13 09:20:28 +0700

    demo

--- selenium/javaTester/topic_02_demo.java
+++ selenium/javaTester/topic_02_demo.java
@@ -1,9 +1,12 @@
 package javaTester;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class topic_02_demo {
+    int x;
 
     public static void main(String[] args) {
-       float x = 3.6f;
+       float  x = 4.5f;
        float y=x;
         System.out.println("x = " + x);
         System.out.println("y = " + y);
@@ -11,6 +14,15 @@ public class topic_02_demo {
         x = 25;
         System.out.println("x = " + x);
         System.out.println("y = " + y);
+
+        topic_02_demo a = new topic_02_demo();
+        a.x = 5;
+        System.out.println("a.x = " + a.x);
+        topic_02_demo d = a;
+        System.out.println("d.x = " + d.x);
+        a.x = 200;
+        System.out.println("a.x = " + a.x);
+        System.out.println("d.x = " + d.x);
     }
 
 }
